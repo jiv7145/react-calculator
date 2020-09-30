c282a142f9962a38c3709fac400fd7441cad605b
Jake Lim <jiv7145@gmail.com>
2020-09-29 19:26:17 -0700

    Updates

---
+++ precache-manifest.453fcb55c6c5cdb74dcb9110788e33fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcefe98feb1d06cf23b1b8ff2ce0d77b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c19cf488df34d5b7c060",
+    "url": "/static/css/main.d66ca807.chunk.css"
+  },
+  {
+    "revision": "ecada51e1a3334c42b99",
+    "url": "/static/js/2.e6f076d8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e6f076d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c19cf488df34d5b7c060",
+    "url": "/static/js/main.9776fab4.chunk.js"
+  },
+  {
+    "revision": "5738b0f7c428695a68f5",
+    "url": "/static/js/runtime-main.2e17527d.js"
+  }
+]);
